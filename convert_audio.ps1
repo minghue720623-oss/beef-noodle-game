23b622db5e71dc99abf84c576afa655eb3a14693
Ahan User <minghue720623@github.com>
2026-04-08 21:13:34 +0800

    Convert all MP3 and WAV files to OGG to support Web Audio

---
+++ convert_audio.ps1
@@ -0,0 +1,15 @@
+$ffmpeg = "C:\Users\User\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-8.1-full_build\bin\ffmpeg.exe"
+
+Get-ChildItem -Filter *.mp3 | ForEach-Object {
+    $out = $_.Name -replace '\.mp3$', '.ogg'
+    Write-Host "Converting $_ to $out"
+    & $ffmpeg -i $_.FullName -c:a libvorbis -q:a 4 $out -y
+    if ($?) { Remove-Item $_.FullName }
+}
+
+Get-ChildItem -Filter *.wav | ForEach-Object {
+    $out = $_.Name -replace '\.wav$', '.ogg'
+    Write-Host "Converting $_ to $out"
+    & $ffmpeg -i $_.FullName -c:a libvorbis -q:a 4 $out -y
+    if ($?) { Remove-Item $_.FullName }
+}
